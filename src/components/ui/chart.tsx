11b5ef84c8239e12af5ddd8303193f840f359828
Kumar Aaryan <sakshamsingh797052@gmail.com>
2026-01-21 16:08:59 +0530

    Fixed legend type error

--- src/components/ui/chart.tsx
+++ src/components/ui/chart.tsx
@@ -277,7 +277,7 @@ function ChartLegendContent({
         className
       )}
     >
-      {payload.map((item) => {
+      {payload.map((item: any) => {
         const key = `${nameKey || item.dataKey || "value"}`
         const itemConfig = getPayloadConfigFromPayload(config, item, key)
 
